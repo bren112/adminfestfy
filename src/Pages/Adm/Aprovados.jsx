8c855be684bd5d8f3d08f49354e63ef922df007d
brenin <130005400+bren112@users.noreply.github.com>
2024-06-13 09:14:59 -0300

    Update Aprovados.jsx

--- src/Pages/Adm/Aprovados.jsx
+++ src/Pages/Adm/Aprovados.jsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { supabase } from '../../Supabase/createClient';
 import './Adm.css';
 
 const Aprovados = () => {
   const [aprovados, setAprovados] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const refs = useRef({});
 
   useEffect(() => {
     fetchAprovados();
   }, []);
+
+  useEffect(() => {
+    const nomeLowerCase = searchTerm.toLowerCase();
+    const matchedIndex = aprovados.findIndex(aprovado => aprovado.nome.toLowerCase().includes(nomeLowerCase));
+    if (matchedIndex !== -1 && refs.current[matchedIndex]) {
+      refs.current[matchedIndex].scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  }, [searchTerm, aprovados]);
 
   const fetchAprovados = async () => {
     try {
@@ -58,44 +67,50 @@ const Aprovados = () => {
       />
       <br />
       <br />
-      <table responsive>
-        <thead>
-          <tr>
-            <th>Número</th>
-            <th>Nome</th>
-            <th>Telefone</th>
-            <th>Valor Pago</th>
-            <th>Entrou na Festa</th>
-            <th>Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          {aprovados.map((aprovado, index) => {
-            const nomeLowerCase = aprovado.nome.toLowerCase();
-            const searchTermLowerCase = searchTerm.toLowerCase();
-            const highlight = searchTerm && nomeLowerCase.includes(searchTermLowerCase) ? 'yellow' : 'inherit';
+      <div id="table-container" style={{ maxHeight: '400px', overflowY: 'auto' }}>
+        <table responsive>
+          <thead>
+            <tr>
+              <th>Número</th>
+              <th>Nome</th>
+              <th>Telefone</th>
+              <th>Valor Pago</th>
+              <th>Entrou na Festa</th>
+              <th>Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {aprovados.map((aprovado, index) => {
+              const nomeLowerCase = aprovado.nome.toLowerCase();
+              const searchTermLowerCase = searchTerm.toLowerCase();
+              const highlight = searchTerm && nomeLowerCase.includes(searchTermLowerCase) ? 'yellow' : 'inherit';
 
-            return (
-              <tr key={aprovado.id} style={{ backgroundColor: highlight }}>
-                <td>{index + 1}</td>
-                <td>{aprovado.nome}</td>
-                <td>{aprovado.telefone}</td>
-                <td>{aprovado.pagamento}</td>
-                <td>
-                  {!aprovado.entrouNaFesta ? (
-                    <button id='entrou' onClick={() => handleEntrouNaFesta(aprovado.id)}>Entrou</button>
-                  ) : (
-                    <span>Sim</span>
-                  )}
-                </td>
-                <td>
-                  <button id='excluir' onClick={() => handleDelete(aprovado.id)}>Excluir</button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+              return (
+                <tr
+                  key={aprovado.id}
+                  ref={el => refs.current[index] = el}
+                  style={{ backgroundColor: highlight }}
+                >
+                  <td>{index + 1}</td>
+                  <td>{aprovado.nome}</td>
+                  <td>{aprovado.telefone}</td>
+                  <td>{aprovado.pagamento}</td>
+                  <td>
+                    {!aprovado.entrouNaFesta ? (
+                      <button id='entrou' onClick={() => handleEntrouNaFesta(aprovado.id)}>Entrou</button>
+                    ) : (
+                      <span>Sim</span>
+                    )}
+                  </td>
+                  <td>
+                    <button id='excluir' onClick={() => handleDelete(aprovado.id)}>Excluir</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
